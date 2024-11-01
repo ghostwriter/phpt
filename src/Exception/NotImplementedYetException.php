61bbf354e49f7882a2dd90ffbe58207277edead5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-31 21:53:00 -0500

    Update NotImplementedYetException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/NotImplementedYetException.php
+++ src/Exception/NotImplementedYetException.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Exception;
 
+use Ghostwriter\Phpt\Interface\ExceptionInterface;
 use LogicException;
 
-final class NotImplementedYetException extends LogicException
+final class NotImplementedYetException extends LogicException implements ExceptionInterface
 {
 }
