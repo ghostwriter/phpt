c4e828641fc30e79b4101773b194bebf7e6b3404
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-31 21:53:13 -0500

    Add FileSectionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Section/FileSectionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phpt\Interface\Section;
+
+use Ghostwriter\Phpt\Interface\SectionInterface;
+
+interface FileSectionInterface extends SectionInterface
+{
+}
