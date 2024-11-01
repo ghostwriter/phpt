b56009019ed7048e32614aedbc9697a6457cc926
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-31 21:53:10 -0500

    Add PhptFileInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/File/PhptFileInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Phpt\Interface\File;
+
+use Ghostwriter\Phpt\Interface;
+
+interface PhptFileInterface extends Interface\FileInterface
+{
+}
