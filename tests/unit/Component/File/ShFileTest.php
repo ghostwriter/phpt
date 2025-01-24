77f81a76666408b6b18bb5d93314f54a4f4ca335
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:37 -0600

    Add ShFileTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Component/File/ShFileTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Component\File;
+
+use Ghostwriter\Phpt\Component\File\ShFile;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(ShFile::class)]
+final class ShFileTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
