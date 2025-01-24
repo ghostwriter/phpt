fb63fbe308c662b9ba6969b02d78c5287efccb3e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:40 -0600

    Update ExpFileTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/File/ExpFileTest.php
+++ tests/unit/File/ExpFileTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\File;
 
-use Ghostwriter\Phpt\File\ExpFile;
+use Ghostwriter\Phpt\Component\File\ExpFile;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
