e8d67e78dc6cd6148e7186ad97326fb39bca89ad
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:38 -0600

    Add PhptCaseTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Component/Test/Case/PhptCaseTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Component\Test\Case;
+
+use Ghostwriter\Phpt\Component\Test\Case\PhptCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(PhptCase::class)]
+final class PhptCaseTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
