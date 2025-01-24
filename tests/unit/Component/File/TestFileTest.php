7723542763f1c5ea27cad45ae1b8f0091a9ce7e5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:37 -0600

    Add TestFileTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/unit/Component/File/TestFileTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Component\File;
+
+use Ghostwriter\Phpt\Component\File\TestFile;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(TestFile::class)]
+final class TestFileTest extends TestCase
+{
+    public function testExample(): void
+    {
+        self::assertTrue(true);
+    }
+}
