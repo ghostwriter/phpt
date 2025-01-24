74b7fd4bae83c46bb25050a6bf78c00e61f3e405
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:42 -0600

    Update ExpectSectionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Section/ExpectSectionTest.php
+++ tests/unit/Section/ExpectSectionTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Section;
 
-use Ghostwriter\Phpt\Section\ExpectSection;
+use Ghostwriter\Phpt\Component\Section\ExpectSection;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
