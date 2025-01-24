f387971dd0bb1890a1def178a0bd4e3e02d87e49
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:41 -0600

    Update PhpFileTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/File/PhpFileTest.php
+++ tests/unit/File/PhpFileTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\File;
 
-use Ghostwriter\Phpt\File\PhpFile;
+use Ghostwriter\Phpt\Component\File\PhpFile;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
