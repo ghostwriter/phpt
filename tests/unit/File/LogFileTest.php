ad7c833217886062e7ef7cb79938d336dff51cca
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:40 -0600

    Update LogFileTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/File/LogFileTest.php
+++ tests/unit/File/LogFileTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\File;
 
-use Ghostwriter\Phpt\File\LogFile;
+use Ghostwriter\Phpt\Component\File\LogFile;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
