5b4de42f86927d21c9d660940d225f92205a9261
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-24 16:30:41 -0600

    Update CreditSectionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Section/CreditSectionTest.php
+++ tests/unit/Section/CreditSectionTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Section;
 
-use Ghostwriter\Phpt\Section\CreditSection;
+use Ghostwriter\Phpt\Component\Section\CreditSection;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
