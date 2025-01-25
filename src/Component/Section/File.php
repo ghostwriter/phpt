f2b73726b93b0dfd83f9499ba84108ccc36bb8a7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:41 -0600

    Move File.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Section/File.php
+++ src/Component/Section/File.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\Section;
 
-final class ExpectSection implements ExpectSectionInterface
+final class File implements FileInterface
 {
     public static function new(): self
     {
