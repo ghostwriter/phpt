489b533f195591aa49045367b133b288fbc74a45
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:43 -0600

    Update PhptTestCaseInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Test/TestCase/PhptTestCaseInterface.php
+++ src/Component/Test/TestCase/PhptTestCaseInterface.php
@@ -6,19 +6,19 @@ namespace Ghostwriter\Phpt\Component\Test\TestCase;
 
 use Ghostwriter\Phpt\Component\File\FileInterface;
 use Ghostwriter\Phpt\Component\Section\SectionInterface;
-use Ghostwriter\Phpt\Component\Section\TestSectionInterface;
+use Ghostwriter\Phpt\Component\Section\TestInterface;
 
 interface PhptTestCaseInterface
 {
     /**
      * @param iterable<FileInterface,SectionInterface> $sections
      */
-    public static function new(TestSectionInterface $testSection, iterable $sections): self;
+    public static function new(TestInterface $test, iterable $sections): self;
 
     /**
      * @return iterable<FileInterface,SectionInterface>
      */
     public function sections(): iterable;
 
-    public function testSection(): TestSectionInterface;
+    public function testSection(): TestInterface;
 }
