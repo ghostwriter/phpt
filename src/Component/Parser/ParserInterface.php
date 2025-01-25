0d7d7430f039974fda2d36e996ca21f8efa0dd19
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:36 -0600

    Update ParserInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Parser/ParserInterface.php
+++ src/Component/Parser/ParserInterface.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\Parser;
 
-use Ghostwriter\Phpt\Component\File\PhptFileInterface;
+use Ghostwriter\Phpt\Component\File\PhptInterface;
 use Ghostwriter\Phpt\Component\Test\Case\PhptCaseInterface;
 
 interface ParserInterface
 {
-    public function parse(PhptFileInterface $phptFile): PhptCaseInterface;
+    public function parse(PhptInterface $phpt): PhptCaseInterface;
 }
