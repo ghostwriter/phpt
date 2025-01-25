7abe3b7cb55436191b26843d41fe3c8eeaddb583
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:58 -0600

    Update TestCasesCollectionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Component/Collection/TestCasesCollectionTest.php
+++ tests/unit/Component/Collection/TestCasesCollectionTest.php
@@ -6,10 +6,10 @@ namespace Tests\Unit\Component\Collection;
 
 use Ghostwriter\Phpt\Component\Collection\TestCasesCollection;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(TestCasesCollection::class)]
-final class TestCasesCollectionTest extends TestCase
+final class TestCasesCollectionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
