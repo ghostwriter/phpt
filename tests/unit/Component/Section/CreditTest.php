a45bf282b5813485c7db097b553d8c7546889b3f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:21:01 -0600

    Move CreditTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/unit/Component/Section/CreditTest.php
+++ tests/unit/Component/Section/CreditTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Component\Section;
 
-use Ghostwriter\Phpt\Component\Section\TestSection;
+use Ghostwriter\Phpt\Component\Section\Credit;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
-#[CoversClass(TestSection::class)]
-final class TestSectionTest extends TestCase
+#[CoversClass(Credit::class)]
+final class CreditTest extends AbstractTestCase
 {
     public function testExample(): void
     {
