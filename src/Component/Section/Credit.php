f9726aa0b3ff02d734547033bab935da724f1f39
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:36 -0600

    Move Credit.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Section/Credit.php
+++ src/Component/Section/Credit.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\Section;
 
-final class TestSection implements TestSectionInterface
+final class Credit implements CreditInterface
 {
     public static function new(): self
     {
