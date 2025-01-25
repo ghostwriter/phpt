49cd2071a7880cd58043385ea70bfe0b8ee393b8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:41 -0600

    Move FileInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Section/FileInterface.php
+++ src/Component/Section/FileInterface.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\Section;
 
-interface SkipIfSectionInterface extends SectionInterface {}
+interface FileInterface extends SectionInterface {}
