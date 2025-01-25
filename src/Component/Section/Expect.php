fae95dc0c5505e7b18d551ddcaf38f52edd5c4d5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:40 -0600

    Move Expect.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Section/Expect.php
+++ src/Component/Section/Expect.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\Section;
 
-final class SkipIfSection implements SkipIfSectionInterface
+final class Expect implements ExpectInterface
 {
     public static function new(): self
     {
