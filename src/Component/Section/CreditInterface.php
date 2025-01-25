8c6b50c872ee8c50e96abbd923ba862e41b96724
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:37 -0600

    Move CreditInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/Section/CreditInterface.php
+++ src/Component/Section/CreditInterface.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\Section;
 
-interface TestSectionInterface extends SectionInterface {}
+interface CreditInterface extends SectionInterface {}
