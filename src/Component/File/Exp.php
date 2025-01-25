a403ed5bed430683b3d59355b43c92c77bc9a36f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-25 12:20:24 -0600

    Move Exp.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Component/File/Exp.php
+++ src/Component/File/Exp.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Phpt\Component\File;
 
-final class LogFile implements LogFileInterface
+final class Exp implements ExpInterface
 {
     public static function new(): self
     {
